6d7308010c91d02ad8b6b06e487d4da17913ad5a
TobyMike-max <osinubi.michael@gmail.com>
2023-02-03 00:45:38 +0100

    [Modified]: Changed Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,20 @@
-import PropTypes from 'prop-types'
 import { useLocation } from 'react-router-dom'
 import Button from './Button'
 
 
-const Header = ({ title, onAdd, showAdd }) => {
+const Header = ({onAdd, showAdd}) => {
   const location = useLocation()
   return(
-    <header className="header">
-    <h1> {title} </h1>
-    {location.pathname === '/' && (
-    <Button color={showAdd ? 'red':'green'}
-    text={showAdd ? 'Close': 'Add'}
-    onClick={onAdd}
-    />
-  )}
-    </header>
+    <>
+      <header className="header">
+        <h1>Hello, Becky</h1>
+      	{location.pathname === '/' && (
+      	<Button color={showAdd ? 'red':'green'}
+      	text={showAdd ? 'Close': 'Add'}
+      	onClick={onAdd}/>)}
+      </header>
+    </>
   )
-}
-
-Header.defaultProps = {
-  title: 'Task tracker'
-}
-
-
-Header.propTypes = {
-  title: PropTypes.string.isRequired,
 }
 
 // CSS in JS
