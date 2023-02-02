59355143fa32e893ff300a8aceae67f7214432a3
TobyMike-max <osinubi.michael@gmail.com>
2023-02-02 16:01:28 +0100

    [Modified]: Added a new function to show real time year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
+  const getDate = () => {
+    const date = new Date();
+    return date.getFullYear();
+  };
+
   return (
     <footer>
-      <p>Copyright &copy; 2021 </p>
+      <p>Copyright &copy; {getDate()}</p>
       <Link to="/about"> About </Link>
     </footer>
   )
